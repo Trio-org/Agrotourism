262fbf5c7d46df2a87edb2ea94e7cc92a2033745
balamurugan634 <bala060304@gmail.com>
2024-07-22 13:49:27 +0530

    tour card ui done

--- client/src/components/TourCard.jsx
+++ client/src/components/TourCard.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import agroimg from '../assets/illustrations/agro1.jpg'
+import { IoLocationOutline } from "react-icons/io5";
+import { FaStar } from "react-icons/fa";
 
-const TourCard = () => {
+
+const TourCard = ({tour}) => {
+
   return (
-    <div>
-      
-    </div>
+  
+    <Link to={ `/details/:${tour.id}`} className='max-w-[90%] sm:max-w-sm  rounded-lg shadow hover:shadow-lg hover:scale-105 transition-all duration-300'>
+      <div className="w-full overflow-hidden "><Link to={'/details/:id'}><img src={tour.pic} alt="" className='rounded-t-lg h-60 w-full hover:scale-105 transition-scale duration-300 ' /></Link></div>
+      <div className="h-1/2 px-3 py-2">
+        <div className="flex justify-between ">
+          <div className=" flex gap-1 items-center"><IoLocationOutline size={17} style={{fill:'gold'}}  />
+          <p>{tour.location}</p></div>
+          <div className="flex gap-1 items-center"><FaStar size={17} style={{fill:'gold'}} />
+          <p>{tour.rating}</p></div>
+        </div>
+        <h1 className='font-semibold text-xl text-[#bbad70]'>{tour.title}</h1>
+        <div className="flex justify-between items-center">
+          <p className='text-sm text-gray-800'><span className='text-lg font-semibold text-green-700'>${tour.amount}</span>/per person</p>
+          <Link to={'/details/:id'} className='bg-[#009a74] bg-opacity-95 hover:bg-opacity-100 text-white text-center font-semibold px-4 py-2 rounded-md'>Book</Link>
+        </div>
+      </div>
+    </Link>
+  
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import TourCard from '../components/TourCard'
+import agroimg from '../assets/illustrations/agro1.jpg'
 
 const Home = () => {
+  // const tours_data=[
+  //   {
+  //     id:1,
+  //     title:"Bankong,Thailand",
+  //     rating:4.5,
+  //     pic:agroimg,
+  //     location:"Thailand",
+  //     amount:99
+  //   },
+  //   {
+  //     id:2,
+  //     title:"Bankong,Japan",
+  //     rating:5,
+  //     pic:agroimg,
+  //     location:"Japan",
+  //     amount:199
+  //   },
+    
+  // ]
   return (
     <div>
-      Home
+      {/* <div className="flex flex-wrap gap-5 justify-center p-3">
+        {tours_data.map((tour)=><TourCard tour={tour}/>)}
+      </div> */}
     </div>
   )
 }
