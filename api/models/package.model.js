12c57d379485ea941e13e1db9ac5892815d2e4a7
balamurugan634 <bala060304@gmail.com>
2024-07-26 22:15:25 +0530

    packagemodel alteration -bala

--- api/models/package.model.js
+++ api/models/package.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 const packageSchema = new mongoose.Schema({
+    booking: { type: mongoose.Schema.Types.ObjectId, ref: 'Farm', required: true },
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
