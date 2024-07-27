f9ba2c0c8c38c7cb5a5012ea2f5a0ba8438dd6dc
balamurugan634 <bala060304@gmail.com>
2024-07-27 16:45:04 +0530

    getallfarms and getallfarms as per user done -bala

--- api/controllers/farm.control.js
+++ api/controllers/farm.control.js
@@ -1,5 +1,5 @@
 import Farm from "../models/farm.model.js";
-
+import mongoose from "mongoose";
 export const farmcreation=async(req,res)=>{
 
    
@@ -63,5 +63,29 @@ export const farmdelete=async(req,res,next)=>{
     } catch (err) {
         console.log(err.message)
         res.status(500).send("server error")
+    }
+}
+export const getfarmlisting=async(req,res,next)=>{
+    try {
+        console.log(req.params.id)
+        const id=new mongoose.Types.ObjectId(req.params.id)
+        // const documentToFind = { _id: new ObjectId(req.params.id) };
+        if(req.params.id !== req.body.curruser.id) return res.status(400).json({msg:"forbidden"})
+        const showlisting=await Farm.find({owner:id})
+        console.log(showlisting)
+        res.status(200).json(showlisting)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send("server error")
+    }
+}
+export const getallfarms=async(req,res,next)=>{
+    try {
+        const allfarms=await Farm.find()
+        res.status(200).json(allfarms)
+    } catch (err) {
+       console.error(err.message)
+        res.status(500).send("server error")
+        
     }
 }
--- api/routers/consumer.route.js
+++ api/routers/consumer.route.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import {verifytoken} from '../utils/verifytoken.js'
 import { getallproducts } from '../controllers/product.control.js'
+import { getallfarms } from '../controllers/farm.control.js'
 const router=express.Router()
 router.get('/getallproducts',getallproducts)
+router.get('/getallfarms',getallfarms)
+
 export default router
