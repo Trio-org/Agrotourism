59be05ac10fc1dca726c869fce45c9e68b73a03e
balamurugan634 <bala060304@gmail.com>
2024-07-27 14:53:21 +0530

    delete product route done -bala

--- api/controllers/product.control.js
+++ api/controllers/product.control.js
@@ -26,8 +26,7 @@ export const productcreation=async(req,res,next)=>{
 export const updateproduct=async(req,res,next)=>{
     try{
     const product =await Product.findById(req.params.id)
-    console.log(product.owner.toString())
-    console.log(req.body.curruser.id)
+    
     if(!product) return res.status(400).json({msg:"product not exist"})
     if(req.body.curruser.id !== product.owner.toString()) return res.status(400).json({msg:"you cannot edit others product"})
     const {name, description, price,offerprice, images } = req.body;
@@ -45,4 +44,18 @@ export const updateproduct=async(req,res,next)=>{
     }
 
 
+}
+
+export const deleteproduct=async(req,res,next)=>{
+    try {
+        const product =await Product.findById(req.params.id)
+    
+    if(!product) return res.status(400).json({msg:"product not exist"})
+    if(req.body.curruser.id !== product.owner.toString()) return res.status(400).json({msg:"you cannot delete others product"})
+    await Product.findByIdAndDelete(req.params.id)
+    res.status(200).json({msg:"product deleted"})
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send("server error")
+    }
 }
