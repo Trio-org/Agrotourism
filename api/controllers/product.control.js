fe8063caa7f203daf5c51449ad6491feb0104431
balamurugan634 <bala060304@gmail.com>
2024-07-27 14:45:30 +0530

    product update route done -bala

--- api/controllers/product.control.js
+++ api/controllers/product.control.js
@@ -22,3 +22,27 @@ export const productcreation=async(req,res,next)=>{
         res.status(500).send('Server error');
     }
 }
+
+export const updateproduct=async(req,res,next)=>{
+    try{
+    const product =await Product.findById(req.params.id)
+    console.log(product.owner.toString())
+    console.log(req.body.curruser.id)
+    if(!product) return res.status(400).json({msg:"product not exist"})
+    if(req.body.curruser.id !== product.owner.toString()) return res.status(400).json({msg:"you cannot edit others product"})
+    const {name, description, price,offerprice, images } = req.body;
+    const updatedproduct=await Product.findByIdAndUpdate(req.params.id,{$set:{
+        name,
+        description,
+        price,
+        offerprice,
+        images
+    }},{new:true})
+    res.status(200).json(updatedproduct)}
+    catch(err){
+        console.error(err.message)
+        res.status(500).send("server error")
+    }
+
+
+}
--- api/routers/product.route.js
+++ api/routers/product.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import {verifytoken} from '../utils/verifytoken.js'
-import { productcreation } from '../controllers/product.control.js'
+import { productcreation, updateproduct } from '../controllers/product.control.js'
 const router=express.Router()
 router.post('/createproduct',verifytoken,productcreation)
-
+// 66a4b4bc9ff4d66dd912f87c -p
+router.post('/updateproduct/:id',verifytoken,updateproduct)
 export default router
